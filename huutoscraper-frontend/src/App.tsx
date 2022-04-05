393c4a64b4d91004a18c1610f8abc8485c8e6f84
Konsta Mustonen <konsta.mustonen@gmail.com>
2022-04-05 17:28:12 +0300

    updated things

--- huutoscraper-frontend/src/App.tsx
+++ huutoscraper-frontend/src/App.tsx
@@ -1,94 +1,97 @@
-import axios from "axios";
-import moment, { Moment } from "moment";
-import { useCallback, useEffect, useState } from "react";
-import "./App.css";
+import axios from 'axios'
+import moment, { Moment } from 'moment'
+import { useCallback, useEffect, useState } from 'react'
+import './App.css'
 
 type Auction = {
-  platform: string;
-  price: string;
-  timeStamp: Moment;
-  title: string;
-  url: string;
-};
+  platform: string
+  price: string
+  timeStamp: Moment
+  title: string
+  url: string
+}
 
 type Config = {
-  targetUrls: string[];
-};
+  targetUrls: string[]
+  blacklist: string[]
+}
 
 const App = () => {
-  const [config, setConfig] = useState<Config>({ targetUrls: [] });
-  const [result, setResult] = useState<Auction[]>([]);
+  const [config, setConfig] = useState<Config>({ targetUrls: [], blacklist: [] })
+  const [result, setResult] = useState<Auction[]>([])
 
   const fetchAll = useCallback(() => {
-    const results: any = {};
-    console.log("do the thing", config);
+    const results: any = {}
     config.targetUrls.forEach((url) => {
       axios
-        .get("http://localhost:3001", {
+        .get('http://localhost:3001', {
           params: {
             url,
           },
         })
         .then((x) => {
           const asd = (x.data as any[]).map((x) => {
-            return { ...x, timeStamp: moment(x.timeStamp) } as Auction;
-          });
-          results[url] = asd;
+            return { ...x, timeStamp: moment(x.timeStamp) } as Auction
+          })
+          results[url] = asd
           if (Object.keys(results).length === config.targetUrls.length) {
-            const flattened: Auction[] =
-              Object.values<Auction>(results).flat<Auction[]>();
-            setResult(flattened);
+            const flattened: Auction[] = Object.values<Auction>(results).flat<Auction[]>()
+            setResult(flattened)
           }
-        });
-    });
-  }, [config]);
+        })
+    })
+  }, [config])
 
   useEffect(() => {
-    fetch("config.json").then((res) => {
+    fetch('config.json').then((res) => {
       res.json().then((config) => {
-        setConfig(config);
-        console.log(config);
-      });
-    });
-    fetchAll();
+        setConfig(config)
+      })
+    })
+  }, [])
+
+  useEffect(() => {
+    fetchAll()
     setInterval(() => {
-      fetchAll();
-    }, 30000);
-  }, [fetchAll]);
+      fetchAll()
+    }, 30000)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [config])
 
   const all = result
-    .sort(
-      (a: Auction, b: Auction) =>
-        b.timeStamp.diff(a.timeStamp) || a.title.localeCompare(b.title)
-    )
-    .filter(
-      (x: Auction) =>
-        x.title && !(x.title as string).toLowerCase().includes("skylanders")
-    );
+    .sort((a: Auction, b: Auction) => b.timeStamp.diff(a.timeStamp) || a.title.localeCompare(b.title))
+    .filter((x: Auction) => {
+      const titleWords = x.title
+        .toLowerCase()
+        .replace(/[^a-zA-Z0-9 ]/g, '')
+        .split(' ')
+      const gotem = titleWords.filter((value) => config.blacklist.includes(value)).length > 0
+      return !gotem
+    })
 
   return (
-    <div className="App">
+    <div className='App'>
       {all.map((x: Auction, i: number) => {
         return (
-          <div key={i} className="row">
-            <div>{x.timeStamp.format("HH:mm")}</div>
+          <div key={i} className='row'>
+            <div>{x.timeStamp.format('HH:mm')}</div>
 
             <div>{x.price}</div>
 
             <div>
-              <img src={`/${x.platform}.png`} alt=""></img>
+              <img src={`/${x.platform}.png`} alt=''></img>
             </div>
 
             <div>
-              <a href={"//" + x.url} target="_blank" rel="noreferrer">
+              <a href={'//' + x.url} target='_blank' rel='noreferrer'>
                 {x.title}
               </a>
             </div>
           </div>
-        );
+        )
       })}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
